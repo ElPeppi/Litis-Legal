7cc16bd5bcc2bdccbd914772e8cfb18b5520efb4
Jefferson Torres <gabrielacarbonell@uninorte.edu.co>
2025-06-09 19:01:49 -0500

    feat: finish

--- js/tabsDisplay.js
+++ js/tabsDisplay.js
@@ -1,8 +1,14 @@
 function showViews(className) {
     const vistas = document.querySelectorAll('.view');
+        const header = document.querySelector('header');
     
     vistas.forEach(v => v.style.display = 'none');
-
+    if(className === 'nosotros-view'){
+        header.style.height = '70vh';
+    }
+    else{
+        header.style.height = '100vh';
+    }
     const vistaActiva = document.querySelectorAll(className);
     vistaActiva.forEach(v => v.style.display = 'block');
 }
